23702d6926d518e8c4cfa938f4f1383a135e834d
vish <vish@VISHNU>
2017-01-02 15:56:37 -0600

    Updated c file

---
+++ Basics/Basics/C_fileread.c
@@ -0,0 +1,24 @@
+#ifdef __cplusplus
+	extern "C"{
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PRINT_TIME(logFileHandle)\
+{\
+	time_t day;
+	struct tm *tm;
+	tm = localtime(&day);
+
+
+}
+
+int main()
+{
+	
+}
+
+#ifdef __cplusplus	
+}
+# endif
