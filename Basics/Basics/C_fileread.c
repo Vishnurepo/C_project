39d5a373251a1acdba76345e3f4dac8c2d124f3e
vish <vish@VISHNU>
2017-01-02 22:25:12 -0600

    Update from VS

--- Basics/Basics/C_fileread.c
+++ Basics/Basics/C_fileread.c
@@ -11,15 +11,27 @@ extern "C" {
 	struct tm *tm;\
 	time(&day);\
 	tm = (localtime(&day));\
-	fprintf(logFileHandle,"\"TIME:\" %02d:%02d:%02d\n\n",tm->tm_hour,tm->tm_min,tm->tm_sec);
-
-
-
+	fprintf(logFileHandle,"\"TIME:\" %02d:%02d:%02d\t",tm->tm_hour,tm->tm_min,tm->tm_sec);\	
 }
 
-int main()
+#define BUFFSIZE 256
+
+int main(int argc, char *argv)
 {
-	
+	FILE *fp = NULL;
+	fp = fopen(argv[1],"r");
+	char *Buffer = NULL;
+	char *tokens = NULL;
+	int numtokens = 0;
+
+	while (!feof(fp))
+	{
+		fgets(Buffer, BUFFSIZE, fp);
+		vistokenize(Buffer, &numtokens, &tokens);
+
+	}
+
+
 }
 
 #ifdef __cplusplus	
