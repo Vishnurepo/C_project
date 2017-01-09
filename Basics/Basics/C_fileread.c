b3c344dcaeccd5790f70be97757c65f671496322
502356753 <502356753@G8SPZYZ1E.logon.ds.ge.com>
2017-01-08 19:18:01 -0600

    from eclipse

--- Basics/Basics/C_fileread.c
+++ Basics/Basics/C_fileread.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv)
 	FILE *fp = NULL;
 	fp = fopen(argv[1],"r");
 	char *Buffer = NULL;
-	char *tokens = NULL;
+	char **tokens = NULL;
 	int numtokens = 0;
 
 	while (!feof(fp))
@@ -37,11 +37,18 @@ int main(int argc, char *argv)
 #ifdef __cplusplus	
 }
 
-int vistokenize(char *line, char **tokens)
+int vistokenize(char *line, char ***tokens)
 {
+	
 	while(strcmp(*line,"\n")!=0 )
 	{
-		line = strchr(line,',');
+		char *dup = NULL;
+		dup = line;
+		line = strchr(dup,',');//line now has new string from ,
+		tokenlength = strlen(dup)-strlen(line);
+		//copy the leftover part to token
+		**tokens = (char**)(malloc(sizeof(char*)+sizeof(**tokens));
+		**tokens[i] = (char*)(malloc(sizeof(char)*tokenlength));
 		
 	}
 }
