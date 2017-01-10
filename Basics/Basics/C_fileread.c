e27f9a7ab0bb50f5a0f80bf2d9dfa2fe73f952c6
vish <vish@VISHNU>
2017-01-09 23:26:14 -0600

    Compiled in Visual Studio

--- Basics/Basics/C_fileread.c
+++ Basics/Basics/C_fileread.c
@@ -4,19 +4,20 @@ extern "C" {
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-#define PRINT_TIME(logFileHandle)\
-{\
+#define PRINT_TIME(logFileHandle) do {\
 	time_t day;\
 	struct tm *tm;\
 	time(&day);\
 	tm = (localtime(&day));\
-	fprintf(logFileHandle,"\"TIME:\" %02d:%02d:%02d\t",tm->tm_hour,tm->tm_min,tm->tm_sec);\	
-}
+	fprintf(logFileHandle,"\"TIME:\" %02d:%02d:%02d\t",tm->tm_hour,tm->tm_min,tm->tm_sec);\
+} while(0)
 
 #define BUFFSIZE 256
+int vistokenize(char *line, char ***tokens);
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 	FILE *fp = NULL;
 	fp = fopen(argv[1],"r");
@@ -27,29 +28,32 @@ int main(int argc, char *argv)
 	while (!feof(fp))
 	{
 		fgets(Buffer, BUFFSIZE, fp);
-		vistokenize(Buffer, &tokens);
+		numtokens = vistokenize(Buffer, &tokens);
 		
 	}
 
-
-}
-
-#ifdef __cplusplus	
+	return 0;
 }
 
 int vistokenize(char *line, char ***tokens)
 {
-	
-	while(strcmp(*line,"\n")!=0 )
+	int  i = 0;
+	while (strcmp(line, "\n") != 0)
 	{
 		char *dup = NULL;
+		int tokenlength = 0;		
+
 		dup = line;
-		line = strchr(dup,',');//line now has new string from ,
-		tokenlength = strlen(dup)-strlen(line);
+		line = strchr(dup, ',');//line now has new string from ,
+		tokenlength = (strlen(dup) - strlen(line));
 		//copy the leftover part to token
-		**tokens = (char**)(malloc(sizeof(char*)+sizeof(**tokens));
-		**tokens[i] = (char*)(malloc(sizeof(char)*tokenlength));
-		
+		*tokens = (char**)(realloc((*tokens),sizeof(char*) + 1));
+		*tokens[i] = (char*)(malloc((sizeof(char)*tokenlength)));
+		++i;
 	}
+	return i;
+}
+
+#ifdef __cplusplus	
 }
 # endif
